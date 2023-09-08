72ff3f19c603a2c2284b41591e84db4872dbec93
flipdeezy <jonmuro@gmail.com>
2023-09-08 01:30:35 -0400

    Release

---
+++ Plugin/dist/deploy-release.ps1
@@ -0,0 +1,17 @@
+﻿New-Item 'G:/Workspace/C++/SFSE_WeaponModelFOV/Plugin/dist/' -ItemType Directory -Force -ErrorAction:SilentlyContinue
+Copy-Item 'G:/Workspace/C++/SFSE_WeaponModelFOV/build/Release/*' 'G:/Workspace/C++/SFSE_WeaponModelFOV/Plugin/dist/' -Force -Recurse -ErrorAction:SilentlyContinue
+New-Item 'H:/SteamLibrary/steamapps/common/Starfield/Data/SFSE/Plugins/' -ItemType Directory -Force -ErrorAction:SilentlyContinue
+Copy-Item 'G:/Workspace/C++/SFSE_WeaponModelFOV/Plugin/dist/*.dll' 'H:/SteamLibrary/steamapps/common/Starfield/Data/SFSE/Plugins/' -Force -Recurse -ErrorAction:SilentlyContinue
+if (Test-Path 'G:/Workspace/C++/SFSE_WeaponModelFOV/Plugin/dist/*.pdb') {
+New-Item 'H:/SteamLibrary/steamapps/common/Starfield/Data/SFSE/Plugins/' -ItemType Directory -Force -ErrorAction:SilentlyContinue
+Copy-Item 'G:/Workspace/C++/SFSE_WeaponModelFOV/Plugin/dist/*.pdb' 'H:/SteamLibrary/steamapps/common/Starfield/Data/SFSE/Plugins/' -Force -Recurse -ErrorAction:SilentlyContinue }
+if (Test-Path 'G:/Workspace/C++/SFSE_WeaponModelFOV/Plugin/dist/*.ini') {
+New-Item 'H:/SteamLibrary/steamapps/common/Starfield/Data/SFSE/Plugins/' -ItemType Directory -Force -ErrorAction:SilentlyContinue
+Copy-Item 'G:/Workspace/C++/SFSE_WeaponModelFOV/Plugin/dist/*.ini' 'H:/SteamLibrary/steamapps/common/Starfield/Data/SFSE/Plugins/' -Force -Recurse -ErrorAction:SilentlyContinue }
+if (Test-Path 'G:/Workspace/C++/SFSE_WeaponModelFOV/Plugin/dist/*.toml') {
+New-Item 'H:/SteamLibrary/steamapps/common/Starfield/Data/SFSE/Plugins/' -ItemType Directory -Force -ErrorAction:SilentlyContinue
+Copy-Item 'G:/Workspace/C++/SFSE_WeaponModelFOV/Plugin/dist/*.toml' 'H:/SteamLibrary/steamapps/common/Starfield/Data/SFSE/Plugins/' -Force -Recurse -ErrorAction:SilentlyContinue }
+if (Test-Path 'G:/Workspace/C++/SFSE_WeaponModelFOV/Plugin/dist/*.json') {
+New-Item 'H:/SteamLibrary/steamapps/common/Starfield/Data/SFSE/Plugins/' -ItemType Directory -Force -ErrorAction:SilentlyContinue
+Copy-Item 'G:/Workspace/C++/SFSE_WeaponModelFOV/Plugin/dist/*.json' 'H:/SteamLibrary/steamapps/common/Starfield/Data/SFSE/Plugins/' -Force -Recurse -ErrorAction:SilentlyContinue }
+Compress-Archive -Path 'G:/Workspace/C++/SFSE_WeaponModelFOV/Plugin/dist/*.dll','G:/Workspace/C++/SFSE_WeaponModelFOV/Plugin/dist/*.ini','G:/Workspace/C++/SFSE_WeaponModelFOV/Plugin/dist/*.json','G:/Workspace/C++/SFSE_WeaponModelFOV/Plugin/dist/*.toml' -DestinationPath 'G:/Workspace/C++/SFSE_WeaponModelFOV/Plugin/dist/WeaponModelFOVSwap.v1.0.0.zip' -Force -ErrorAction:SilentlyContinue
---
+++ Plugin/src/settings.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class Settings : public ISingleton<Settings>
+{
+public:
+    void LoadSettings();
+    float weaponFOV{ 120.0f };
+};
